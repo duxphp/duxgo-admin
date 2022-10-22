d54bc960a114ec0607c8f9b38798595d688c5e08
mr.l <admin@duxphp.com>
2022-10-22 16:48:15 +0800

    增加地区级别参数

--- tools/service/area.go
+++ tools/service/area.go
@@ -8,6 +8,7 @@ import (
 	"github.com/duxphp/duxgo/response"
 	"github.com/duxphp/duxgo/util/function"
 	"github.com/labstack/echo/v4"
+	"github.com/spf13/cast"
 )
 
 func AreaTree(ctx echo.Context) error {
@@ -17,8 +18,12 @@ func AreaTree(ctx echo.Context) error {
 		Id       string `json:"id"`
 		ParentId string `json:"parent_id"`
 	}
+	level := cast.ToUint(ctx.QueryParam("level"))
+	if level == 0 {
+		level = 2
+	}
 	results := []Result{}
-	err := core.Db.Model(&model.ToolDistrict{}).Select("name as label", "name as value", "id", "parent_id").Where("level <= ?", 2).Find(&results).Error
+	err := core.Db.Model(&model.ToolDistrict{}).Select("name as label", "name as value", "id", "parent_id").Where("level <= ?", level).Find(&results).Error
 	if err != nil {
 		return exception.Internal(err)
 	}
