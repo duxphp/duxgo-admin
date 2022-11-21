2178a7031081d8b408078bfd7f96ddf50263a7e8
mr.l <admin@duxphp.com>
2022-11-21 20:00:31 +0800

    新增配置保存功能

--- system/service/config.go
+++ system/service/config.go
@@ -6,21 +6,21 @@ import (
 )
 
 // ConfigGet 获取配置
-func ConfigGet(hasType string, hasId string) map[string]any {
+func ConfigGet(hasType string, hasId uint) map[string]any {
 	info := model.SystemConfig{}
 	core.Db.Model(model.SystemConfig{}).Where("has_type", hasType).Where("has_id", hasId).Find(&info)
 	return info.Data
 }
 
 // ConfigGetValue 获取配置值
-func ConfigGetValue[T any](hasType string, hasId string, key string) any {
+func ConfigGetValue[T any](hasType string, hasId uint, key string) any {
 	info := model.SystemConfig{}
 	core.Db.Model(model.SystemConfig{}).Where("has_type", hasType).Where("has_id", hasId).Find(&info)
 	return info.Data[key]
 }
 
 // ConfigSave 保存配置
-func ConfigSave(hasType string, hasId string, data map[string]any) {
+func ConfigSave(hasType string, hasId uint, data map[string]any) {
 	info := model.SystemConfig{}
 	core.Db.Model(model.SystemConfig{}).Where("has_type", hasType).Where("has_id", hasId).Find(&info)
 	info.Data = data
@@ -28,7 +28,7 @@ func ConfigSave(hasType string, hasId string, data map[string]any) {
 }
 
 // ConfigSaveValue 保存配置值
-func ConfigSaveValue(hasType string, hasId string, key string, value any) {
+func ConfigSaveValue(hasType string, hasId uint, key string, value any) {
 	info := model.SystemConfig{}
 	core.Db.Model(model.SystemConfig{}).Where("has_type", hasType).Where("has_id", hasId).Find(&info)
 	info.Data[key] = value
