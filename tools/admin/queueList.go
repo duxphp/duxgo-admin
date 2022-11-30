1a26272f61ace495da7f948928e54155a9f47089
mr.l <admin@duxphp.com>
2022-11-30 17:44:47 +0800

    update

--- tools/admin/queueList.go
+++ tools/admin/queueList.go
@@ -2,7 +2,6 @@ package admin
 
 import (
 	"github.com/duxphp/duxgo-admin/system/service"
-	"github.com/duxphp/duxgo-ui/lib/form"
 	"github.com/duxphp/duxgo-ui/lib/table"
 	"github.com/duxphp/duxgo-ui/lib/table/column"
 	"github.com/duxphp/duxgo/core"
@@ -24,24 +23,24 @@ func QueueAjax(ctx echo.Context) error {
 }
 
 func queueTable(ctx echo.Context) *table.Table {
-	table := table.NewTable()
+	tableT := table.NewTable()
 
-	table.SetUrl("/admin/tools/queueList/ajax")
+	tableT.SetUrl("/admin/tools/queueList/ajax")
 
-	table.AddTab("处理中")
-	table.AddTab("待处理")
-	table.AddTab("定时任务")
-	table.AddTab("重试任务")
-	table.AddTab("存档任务")
+	tableT.AddTab("处理中")
+	tableT.AddTab("待处理")
+	tableT.AddTab("定时任务")
+	tableT.AddTab("重试任务")
+	tableT.AddTab("存档任务")
 
 	queueList, _ := core.QueueInspector.Queues()
 	options := map[any]any{}
 	for _, name := range queueList {
 		options[name] = name
 	}
-	table.AddFilter("队列类型", "queue").SetUI(form.NewSelect().SetOptions(options)).SetQuick(true).SetDefault("default")
+	//tableT.AddFilter("队列类型", "queue").SetUI(form.NewSelect().SetOptions(options)).SetQuick(true).SetDefault("default")
 
-	table.SetDataFun(func(filter map[string]any) (collect collection.ICollection) {
+	tableT.SetDataFun(func(filter map[string]any) (collect collection.ICollection) {
 		qname := filter["queue"].(string)
 		var tasks []*asynq.TaskInfo
 		if filter["tab"] == 0 {
@@ -91,13 +90,11 @@ func queueTable(ctx echo.Context) *table.Table {
 		return collection.NewObjCollection(data)
 	}, "id")
 
-	table.AddCol("ID", "FamilyId").SetUI(column.NewContext())
-	table.AddCol("类型", "Type").SetUI(column.NewContext())
-	table.AddCol("参数", "Payload").SetUI(column.NewContext())
+	tableT.AddCol("ID", "FamilyId").SetUI(column.NewContext())
+	tableT.AddCol("类型", "Type").SetUI(column.NewContext())
+	tableT.AddCol("参数", "Payload").SetUI(column.NewContext())
 
-	table.AddCol("时间", "Time").SetUI(column.NewContext()).SetWidth(220)
+	tableT.AddCol("时间", "Time").SetUI(column.NewContext()).SetWidth(220)
 
-	// 排序规则
-
-	return table
+	return tableT
 }
